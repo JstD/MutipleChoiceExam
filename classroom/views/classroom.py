f2fc0ec9a7c468819304e9428fa1f0f005be99fa
longbui <buingohoanglong2000@gmail.com>
2020-12-23 20:26:32 +0700

    update login

--- classroom/views/classroom.py
+++ classroom/views/classroom.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+from django.http import *
 
 
 class SignUpView(TemplateView):
@@ -9,7 +10,9 @@ class SignUpView(TemplateView):
 def home(request):
     if request.user.is_authenticated:
         if request.user.is_teacher:
-            return redirect('teachers:quiz_change_list')
+            # return redirect('teachers:quiz_change_list')
+            return HttpResponse("Student " + request.user.username + "login succesfully")
         else:
-            return redirect('students:quiz_list')
+            # return redirect('students:quiz_list')
+            return HttpResponse("Teacher" + request.user.username + "login succesfully")
     return render(request, 'classroom/home.html')
