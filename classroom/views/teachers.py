bdefa2858b9e3da9485c3960624463763b86f52d
Le Thanh Son <son.le14133@hcmut.edu.vn>
2020-12-29 16:22:05 +0700

    View participator point

--- classroom/views/teachers.py
+++ classroom/views/teachers.py
@@ -152,8 +152,39 @@ class ExamListView(ListView):
 
         for i in range(len(taken_exam)-1):
             final_res = taken_exam[i] | taken_exam[i+1]
-        print(final_res)
-        context["taker"] = final_res
+
+        info = []
+        for takeexam in final_res:
+            mark = 0
+            # print(len(takeexam.exam.questionpresentation_set.all()))
+            for question_presentation in takeexam.exam.questionpresentation_set.all():
+                for answerorder in question_presentation.answerorder_set.all():
+                    if answerorder.option == answerorder.answerid.answerid and answerorder.answerid.result == True:
+                        mark = mark + 1
+
+                # class Result(object):
+                #     pass
+                #
+                # question_list = []
+                # result = Result()
+                # result.order = question_presentation.number
+                # result.question = question_presentation.question
+                # # result.answer_a = question_presentation.question.answerpart_set.get(answerid='A')
+                # # result.answer_b = question_presentation.question.answerpart_set.get(answerid='B')
+                # # result.answer_c = question_presentation.question.answerpart_set.get(answerid='C')
+                # # result.answer_d = question_presentation.question.answerpart_set.get(answerid='D')
+                # result.student_choice = question_presentation.answerorder_set.all()[0].option
+                # question_list.append(result)
+
+            total = len(takeexam.exam.questionpresentation_set.all())
+
+            score_based_10 = float(mark/total) *100
+            taker = (takeexam, mark, total, score_based_10)
+
+            info.append(taker)
+
+        # context["taker"] = final_res
+        context["taker"] = info
         context["form"] = ExamAddForm()
         return context
 
